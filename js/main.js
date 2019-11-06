450d547e644dfa09606d9567b90e2902f9fffd0a
iths-anna-boberg <amboberg@gmail.com>
2019-11-06 21:42:09 +0100

    started working on add to cart

--- js/main.js
+++ js/main.js
@@ -6,21 +6,60 @@ class Tshirt{
     }
 }
 
-let shoppingCart = []
+// let shoppingCart = JSON.parse(localStorage.getItem("cartContent"))
 
-function colorSelector(){
-    let selector = document.querySelector("select");
-    let box = document.querySelector("#t-shirt-box")
 
-    selector.addEventListener("change", (event)=>{
-        let color = event.target.value
-        if(color =="B"){
-            box.className = "t-shirt-b"
-        }else{
-            box.className = "t-shirt-w"
-        }
 
-    })
+
+function Shop(){
+    return{
+        noSizePicked: "Please choose a size.",
+        shoppingCart: JSON.parse(localStorage.getItem("cartContent"))
+    },
+
+    function updateShoppingCart(){
+        this.shoppingCart = JSON.parse(localStorage.getItem("cartContent"))
+    },
+
+    function colorSelector(){
+        let selector = document.querySelector("#color-selector");
+        let box = document.querySelector("#t-shirt-box")
+    
+        selector.addEventListener("change", event =>{
+            let color = event.target.value
+            if(color =="B"){
+                box.className = "t-shirt-b"
+            }else{
+                box.className = "t-shirt-w"
+            }
+    
+        })
+    },
+
+    function addToCart(){
+        let size = document.querySelectorAll("#size-selector").value
+        let teeShirt = document.querySelector("#t-shirt-box")
+        let addToCartBtn = document.querySelector("#add-to-cart-btn")
+        let color = "white"
+        let cartItems = []
+        
+        addToCartBtn.addEventListener("click", event=>{
+            if(size =="not"){
+                console.log(noSizePicked)
+            }else{
+                let teeText = document.querySelector(".text-box").innerText
+                if(teeShirt.className == "t-shirt-b"){
+                    color = "black"
+                }
+                let item = new Tshirt(color, teeText, size)
+                cartItems.push(item)
+                localStorage.setItem("cartContent", JSON.stringify(cartItems))
+                this.updateShoppingCart()
+                console.log(shoppingCart)
+            }
+        })
+    }
+
 }
-
-colorSelector();
+shop = new Shop();
+shop();
