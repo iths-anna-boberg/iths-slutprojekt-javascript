ba7ca8eb945c0c9c6e68b9956ba70407196aed5e
iths-anna-boberg <amboberg@gmail.com>
2019-11-07 11:57:40 +0100

    cart
    påbörjat hur varukorgen ser ut

--- js/main.js
+++ js/main.js
@@ -6,60 +6,63 @@ class Tshirt{
     }
 }
 
-// let shoppingCart = JSON.parse(localStorage.getItem("cartContent"))
-
-
-
 
 function Shop(){
     return{
-        noSizePicked: "Please choose a size.",
-        shoppingCart: JSON.parse(localStorage.getItem("cartContent"))
-    },
-
-    function updateShoppingCart(){
-        this.shoppingCart = JSON.parse(localStorage.getItem("cartContent"))
-    },
-
-    function colorSelector(){
-        let selector = document.querySelector("#color-selector");
-        let box = document.querySelector("#t-shirt-box")
-    
-        selector.addEventListener("change", event =>{
-            let color = event.target.value
-            if(color =="B"){
-                box.className = "t-shirt-b"
-            }else{
-                box.className = "t-shirt-w"
-            }
-    
-        })
-    },
-
-    function addToCart(){
-        let size = document.querySelectorAll("#size-selector").value
-        let teeShirt = document.querySelector("#t-shirt-box")
-        let addToCartBtn = document.querySelector("#add-to-cart-btn")
-        let color = "white"
-        let cartItems = []
+        shoppingCart: JSON.parse(localStorage.getItem("cartContent")),
         
-        addToCartBtn.addEventListener("click", event=>{
-            if(size =="not"){
-                console.log(noSizePicked)
-            }else{
-                let teeText = document.querySelector(".text-box").innerText
-                if(teeShirt.className == "t-shirt-b"){
-                    color = "black"
-                }
-                let item = new Tshirt(color, teeText, size)
-                cartItems.push(item)
-                localStorage.setItem("cartContent", JSON.stringify(cartItems))
-                this.updateShoppingCart()
-                console.log(shoppingCart)
-            }
-        })
-    }
+        updateShoppingCart(){
+            let counter = document.querySelector("#counter")
+            this.shoppingCart = JSON.parse(localStorage.getItem("cartContent"))
+            counter.innerText = this.shoppingCart.length
+        },
 
+        colorSelector(){
+            let selector = document.querySelector(".color-selector");
+            let box = document.querySelector("#t-shirt-box")
+    
+            selector.addEventListener("change", event =>{
+                let color = event.target.value
+                console.log(color)
+    
+                if(color =="B"){
+                    box.className = "t-shirt-b"
+                }else{
+                    box.className = "t-shirt-w"
+                }
+        
+            })
+        },
+
+        addToCart(){
+            let teeShirt = document.querySelector("#t-shirt-box")
+            let addToCartBtn = document.querySelector("#add-to-cart-btn")
+            let color = "white"
+            let noSizePicked = "Please choose a size."
+            let cartItems = this.shoppingCart
+            
+            addToCartBtn.addEventListener("click", event=>{
+                let size = document.querySelector("#size-selector").value
+                // size = size.value
+                if(size =="not"){
+                    console.log(noSizePicked)
+                }else{
+                    let teeText = document.querySelector(".text-box").innerText
+                    if(teeShirt.className == "t-shirt-b"){
+                        color = "black"
+                    }
+                    let item = new Tshirt(color, teeText, size)
+                    cartItems.push(item)
+                    localStorage.setItem("cartContent", JSON.stringify(cartItems))
+                    this.updateShoppingCart()
+                    console.log(this.shoppingCart)
+                }
+            })
+        }
+    }
 }
-shop = new Shop();
-shop();
+
+let shop = new Shop();
+shop.updateShoppingCart();
+shop.colorSelector();
+shop.addToCart();
